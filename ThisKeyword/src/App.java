a78bb6eea92485dfed73f0a1e3c1d928ff302e01
Adil jaseem <65609243+jaseem1999@users.noreply.github.com>
2021-06-16 21:32:02 +0530

    Add files via upload

---
+++ ThisKeyword/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        sample s = new sample( 10, 10);
+        
+    }
+ 
+    
+}
---
+++ ThisKeyword/src/sample.java
@@ -0,0 +1,13 @@
+public class sample {
+   
+    int a = 200, b = 300;
+
+    sample(int a, int b){
+        this.a = a;
+        this.b = b;
+        System.out.println("it is used this keyword "+this.a+" "+this.b);
+        
+    }
+   
+    
+}
